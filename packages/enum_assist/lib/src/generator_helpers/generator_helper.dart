94d96ecfac4b0f7ab4b214f71eaf18c1ee8ce955
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-11 13:17:16 -0500

    style: update imports & docs

---
+++ packages/enum_assist/lib/src/generator_helpers/generator_helper.dart
@@ -0,0 +1,29 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:enum_assist/src/configs/class_config.dart';
+import 'package:enum_assist/src/generator_helpers/extension_generator_helper.dart';
+import 'package:enum_assist/src/generator_helpers/helper_core.dart';
+import 'package:enum_assist/src/settings.dart';
+import 'package:source_gen/source_gen.dart';
+
+/// {@template enum_assist.enum_generator}
+/// A helper class for generating code for enums.
+/// {@endtemplate}
+class GeneratorHelper extends HelperCore with ExtensionGeneratorHelper {
+  /// {@macro enum_assist.enum_generator}
+  GeneratorHelper(
+    Settings generator,
+    ClassElement element,
+    ConstantReader annotation,
+  ) : super(
+          element,
+          ClassConfig.mergeConfigs(
+            generator.config,
+            annotation,
+          ),
+        );
+
+  @override
+  Iterable<String> generate() sync* {
+    yield generateExtensions().toString();
+  }
+}
