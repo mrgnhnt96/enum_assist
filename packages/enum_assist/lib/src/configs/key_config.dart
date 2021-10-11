705792f0ff6916461bb7ea7c0d89336a4838ee5a
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-11 13:12:31 -0500

    doc: update doc comments

--- packages/enum_assist/lib/src/configs/key_config.dart
+++ packages/enum_assist/lib/src/configs/key_config.dart
@@ -1,7 +1,10 @@
 import 'package:enum_assist_annotation/enum_assist_annotation.dart';
 
+/// {@template enum_assist.key_config}
 /// Represents values from [EnumKey] when merged with local configuration.
+/// {@endtemplate}
 class KeyConfig implements EnumKey {
+  /// {@macro enum_assist.key_config}
   const KeyConfig({
     required this.description,
     required this.name,
