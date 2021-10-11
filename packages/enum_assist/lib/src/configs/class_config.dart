d6f0f7b3c64f8117e3505a357f67c8fc1842a41d
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-11 13:12:14 -0500

    refactor: update value for annotation
    update organization of file

--- packages/enum_assist/lib/src/configs/class_config.dart
+++ packages/enum_assist/lib/src/configs/class_config.dart
@@ -1,24 +1,40 @@
-import 'package:analyzer/dart/element/element.dart';
 import 'package:enum_assist_annotation/enum_assist_annotation.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:source_gen/source_gen.dart';
 
 part 'class_config.g.dart';
 
+/// {@template enum_assist.class_config}
 /// Represents values from [EnumAssist] when merged with local
 /// configuration.
 ///
 /// Values are all known, so types are non-nullable.
+/// {@endtemplate}
 @JsonSerializable(
   fieldRename: FieldRename.snake,
   disallowUnrecognizedKeys: true,
 )
 class ClassConfig implements EnumAssist {
+  /// {@macro enum_assist.class_config}
   const ClassConfig({
     required this.createJsonConv,
     required this.fieldFormat,
     required this.useDocCommentAsDescription,
   });
+
+  /// Merges [config] with [reader].annotation
+  ///
+  /// priority is given to `annotation`
+  factory ClassConfig.mergeConfigs(ClassConfig config, ConstantReader reader) {
+    final annotation = _valueForAnnotation(reader);
+
+    return ClassConfig(
+      createJsonConv: annotation.createJsonConv ?? config.createJsonConv,
+      fieldFormat: annotation.fieldFormat ?? config.fieldFormat,
+      useDocCommentAsDescription: annotation.useDocCommentAsDescription ??
+          config.useDocCommentAsDescription,
+    );
+  }
 
   @override
   final bool createJsonConv;
@@ -29,37 +45,35 @@ class ClassConfig implements EnumAssist {
   @override
   final bool useDocCommentAsDescription;
 
+  /// all the default values for [ClassConfig]
   static const defaults = ClassConfig(
     createJsonConv: true,
     fieldFormat: FieldFormat.none,
     useDocCommentAsDescription: true,
   );
-
-  factory ClassConfig.mergeConfigs(
-    ClassConfig config,
-    ConstantReader reader, {
-    required ClassElement classElement,
-  }) {
-    final annotation = _valueForAnnotation(reader);
-
-    return ClassConfig(
-      createJsonConv: annotation.createJsonConv ?? config.createJsonConv,
-      fieldFormat: annotation.fieldFormat ?? config.fieldFormat,
-      useDocCommentAsDescription: annotation.useDocCommentAsDescription ??
-          config.useDocCommentAsDescription,
-    );
-  }
 }
 
 EnumAssist _valueForAnnotation(ConstantReader reader) {
-  final fieldFormatConv = FieldFormatConv();
+  const fieldFormatConv = FieldFormatConv();
+
+  FieldFormat? fieldFormat;
+  final fieldFormatJson =
+      reader.read(EnumAssist.fields.fieldFormat).literalValue as String?;
+
+  if (fieldFormatJson != null) {
+    fieldFormat = fieldFormatConv.fromJson(fieldFormatJson);
+  }
+
+  final createJsonConv =
+      reader.read(EnumAssist.fields.createJsonConv).literalValue as bool?;
+
+  final useDocCommentAsDescription = reader
+      .read(EnumAssist.fields.useDocCommentAsDescription)
+      .literalValue as bool?;
+
   return EnumAssist(
-    createJsonConv:
-        reader.read(EnumAssist.fields.createJsonConv).literalValue as bool,
-    fieldFormat: fieldFormatConv.fromJson(
-        reader.read(EnumAssist.fields.fieldFormat).literalValue as String),
-    useDocCommentAsDescription: reader
-        .read(EnumAssist.fields.useDocCommentAsDescription)
-        .literalValue as bool,
+    createJsonConv: createJsonConv,
+    fieldFormat: fieldFormat,
+    useDocCommentAsDescription: useDocCommentAsDescription,
   );
 }
