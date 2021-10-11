3624e2416f4d962fd848783103c0364558a6d656
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-11 17:20:09 -0500

    feat: create json_converter template

---
+++ packages/enum_assist/lib/src/templates/json_converter_template.dart
@@ -0,0 +1,89 @@
+import 'package:enum_assist/src/enum_field.dart';
+import 'package:enum_assist/src/templates/template_core.dart';
+import 'package:enum_assist/src/util/string_helpers.dart';
+import 'package:enum_assist/src/util/util.dart';
+
+/// {@template enum_assist.map_template}
+/// Returns the map extension template
+/// {@endtemplate}
+class JsonConverterTemplate extends TemplateCoreDetailed<_MapItem> {
+  /// {@macro enum_assist.map_template}
+  JsonConverterTemplate(String enumName, Iterable<EnumField> fields)
+      : super(enumName, fields);
+
+  @override
+  StringBuffer writeTemplate(StringBuffer buffer) {
+    buffer.writeobj(
+      'class ${enumName}Conv extends JsonConverter<$enumName, String>',
+      body: (classBuff, classTab) {
+        classBuff
+          ..writelnTab('const ${enumName}Conv();\n', classTab)
+          ..writeln(map((i) => tab(i.privateFieldGetter, classTab)))
+          ..writeln()
+          ..writelnTab('@override', classTab)
+          ..writeobj(
+            '$enumName fromJson(String json)',
+            tab: classTab,
+            body: (fromBuff, fromTab) {
+              fromBuff.writeobj(
+                'switch (json)',
+                tab: fromTab,
+                body: (switchBuff, switchTab) {
+                  String fromCase(_MapItem i) {
+                    String _tab([int n = 0]) => tab('', switchTab + n);
+
+                    return i.fromCaseItem(_tab(), _tab(1));
+                  }
+
+                  switchBuff
+                    ..writeln(map(fromCase))
+                    ..writelnTab('default:', switchTab)
+                    ..writelnTab(
+                      r"throw Exception('Unknown field format: $json');",
+                      switchTab + 1,
+                    );
+                },
+              );
+            },
+          )
+          ..writeln()
+          ..writelnTab('@override', classTab)
+          ..writeobj(
+            'String toJson($enumName object) => object.map',
+            tab: classTab,
+            includeSpaceBetweenOpen: false,
+            open: '(',
+            close: ');',
+            body: (toBuff, toTab) {
+              String toCase(_MapItem i) => tabn(i.toReturnString, toTab + 2);
+
+              toBuff.writeln(map(toCase));
+            },
+          );
+      },
+    );
+
+    return buffer;
+  }
+
+  @override
+  _MapItem convert(EnumField e) => _MapItem(enumName, e);
+}
+
+class _MapItem extends FieldTemplate<EnumField> {
+  const _MapItem(String enumName, EnumField field) : super(enumName, field);
+
+  String get privateField => '_${field}Name';
+  String get privateFieldGetter =>
+      "static const $privateField = '${field.getSerializedName}';";
+
+  String get arg => 'required T $field,';
+  String caseString(String str) => 'case $str:';
+
+  String get toReturnString => '$field: $privateField,';
+
+  String get fromReturnString => 'return ${field.wholeName};';
+  String fromCaseItem(String caseTab, String returnTab) => '''
+$caseTab${caseString(privateField)}
+$returnTab$fromReturnString''';
+}
