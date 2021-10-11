d8cb4db67b3d35b2afa4e7c2a120f5bbcbaaad78
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-11 17:23:57 -0500

    feat: add json_annotation as export for JsonConverter

--- packages/enum_assist_annotation/lib/enum_assist_annotation.dart
+++ packages/enum_assist_annotation/lib/enum_assist_annotation.dart
@@ -1,5 +1,7 @@
 library enum_assist_annotation;
 
+export 'package:json_annotation/json_annotation.dart';
+
 export 'src/enum_assist.dart';
 export 'src/enum_key.dart';
 export 'src/util/converters.dart';
