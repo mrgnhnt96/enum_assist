62ae55ce0320e335b487eab7394081c7a98b7c60
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-11 17:17:44 -0500

    feat: add from settings, manual constructors

--- packages/enum_assist/lib/src/configs/key_config.dart
+++ packages/enum_assist/lib/src/configs/key_config.dart
@@ -1,7 +1,12 @@
+import 'package:analyzer/dart/constant/value.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:enum_assist/src/configs/class_config.dart';
+import 'package:enum_assist/src/configs/configs.dart';
 import 'package:enum_assist_annotation/enum_assist_annotation.dart';
+import 'package:source_gen/source_gen.dart';
 
 /// {@template enum_assist.key_config}
-/// Represents values from [EnumKey] when merged with local configuration.
+/// Represents values from the [EnumKey] annotation.
 /// {@endtemplate}
 class KeyConfig implements EnumKey {
   /// {@macro enum_assist.key_config}
@@ -11,6 +16,33 @@ class KeyConfig implements EnumKey {
     required this.serializedValue,
     required this.useDocCommentAsDescription,
   });
+
+  /// {@macro enum_assist.key_config}
+  KeyConfig.fromSettings(KeyConfig settings)
+      : this(
+          description: settings.description,
+          name: settings.name,
+          serializedValue: settings.serializedValue,
+          useDocCommentAsDescription: settings.useDocCommentAsDescription,
+        );
+
+  /// Constructs the key configs from the [EnumKey] annotation.
+  ///
+  /// {@macro enum_assist.key_config}
+  factory KeyConfig.mergeConfigs(
+    ClassConfig classConfig,
+    FieldElement element,
+  ) {
+    final result = _getConfigFromAnnotation(element);
+
+    return KeyConfig(
+      name: result.name,
+      description: result.description,
+      serializedValue: result.serializedValue,
+      useDocCommentAsDescription: result.useDocCommentAsDescription ??
+          classConfig.useDocCommentAsDescription,
+    );
+  }
 
   @override
   final String? description;
@@ -23,4 +55,48 @@ class KeyConfig implements EnumKey {
 
   @override
   final bool useDocCommentAsDescription;
+
+  /// all the default values of [EnumKey]
+  static const defaults = KeyConfig(
+    name: null,
+    description: null,
+    serializedValue: null,
+    useDocCommentAsDescription: true,
+  );
+
+  static const _enumKeyChecker = TypeChecker.fromRuntime(EnumKey);
+
+  static EnumKey _getConfigFromAnnotation(FieldElement element) {
+    DartObject? object;
+    object = _enumKeyChecker.firstAnnotationOf(element);
+
+    if (object == null) {
+      if (element.getter != null) {
+        object = _enumKeyChecker.firstAnnotationOf(element.getter!);
+      }
+    }
+
+    if (object == null) return KeyConfig.defaults;
+
+    final reader = ConstantReader(object);
+
+    final name = reader.read(EnumKey.fields.name).literalValue as String?;
+
+    final description =
+        reader.read(EnumKey.fields.description).literalValue as String?;
+
+    final serializedValue =
+        reader.read(EnumKey.fields.serializedValue).literalValue as String?;
+
+    final useDocCommentAsDescription = reader
+        .read(EnumKey.fields.useDocCommentAsDescription)
+        .literalValue as bool?;
+
+    return EnumKey(
+      name: name,
+      description: description,
+      serializedValue: serializedValue,
+      useDocCommentAsDescription: useDocCommentAsDescription,
+    );
+  }
 }
