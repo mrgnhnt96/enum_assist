8d0673b5c837a6e036828b4c30be5f660c7aa601
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-11 13:14:20 -0500

    docs: update doc comments

--- packages/enum_assist/lib/src/util/exceptions.dart
+++ packages/enum_assist/lib/src/util/exceptions.dart
@@ -1,11 +1,22 @@
+// ignore_for_file: comment_references
+
+/// {@template enum_assist.exception}
+/// The exception thrown by the [EnumAssist] class.
+/// {@endtemplate}
 class CheckedFromEnumAssistException implements Exception {
+  /// {@macro enum_assist.exception}
   const CheckedFromEnumAssistException({
     this.key,
     this.message,
     this.innerError,
   });
 
+  /// The key of the exception thrown.
   final String? key;
+
+  /// The message of the exception.
   final String? message;
+
+  /// Any inner error that caused the exception.
   final Exception? innerError;
 }
