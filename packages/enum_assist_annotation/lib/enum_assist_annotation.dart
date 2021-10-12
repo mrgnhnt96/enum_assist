48c74b133181ef1fcf7dda94aea9dd25a8b9e940
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-12 15:45:31 -0500

    refactor: update imports

--- packages/enum_assist_annotation/lib/enum_assist_annotation.dart
+++ packages/enum_assist_annotation/lib/enum_assist_annotation.dart
@@ -2,9 +2,12 @@ library enum_assist_annotation;
 
 export 'package:json_annotation/json_annotation.dart';
 
-export 'src/additional_method.dart';
+export 'src/additional_extension.dart';
 export 'src/enum_assist.dart';
 export 'src/enum_key.dart';
+export 'src/extension_value.dart';
+export 'src/map_extension.dart';
+export 'src/maybe_map_extension.dart';
 export 'src/util/converters.dart';
 export 'src/util/enums.dart';
 export 'src/util/extensions.dart';
