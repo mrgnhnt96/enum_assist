11897552256e3e4f0b1d023a14a1db2e7c2e67f9
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-12 15:51:11 -0500

    feat: add fromJson (resolve)

--- packages/enum_assist/lib/src/settings.dart
+++ packages/enum_assist/lib/src/settings.dart
@@ -6,19 +6,51 @@ import 'package:enum_assist_annotation/enum_assist_annotation.dart';
 /// {@endtemplate}
 class Settings {
   /// {@macro enum_assist.settings}
-  const Settings({EnumAssist? config})
-      : _config = config ?? ClassConfig.defaults;
+  const Settings._(this._config);
 
-  final EnumAssist _config;
+  /// Resolve the settings from the build.yaml file.
+  ///
+  /// {@macro enum_assist.settings}
+  factory Settings.resolve(Map<String, dynamic> json) {
+    const createJsonConvKey = 'create_json_conv';
+    const fieldFormatKey = 'field_format';
+    const useDocCommentAsDescriptionKey = 'use_doc_comment_as_description';
+    const requiredExtensionsKey = 'required_extensions';
 
-  /// retrieves the [EnumAssist] configuration
-  ClassConfig get config => ClassConfig(
-        createJsonConv:
-            _config.createJsonConv ?? ClassConfig.defaults.createJsonConv,
-        fieldFormat: _config.fieldFormat ?? ClassConfig.defaults.fieldFormat,
-        useDocCommentAsDescription: _config.useDocCommentAsDescription ??
-            ClassConfig.defaults.useDocCommentAsDescription,
-        additionalMethods:
-            _config.additionalMethods ?? ClassConfig.defaults.additionalMethods,
-      );
+    final defaultVal = ClassConfig.defaults;
+
+    final createJsonConvValue =
+        json[createJsonConvKey] as bool? ?? defaultVal.createJsonConv;
+
+    final fieldFormatString = json[fieldFormatKey] as String?;
+
+    const fieldFormatConv = FieldFormatConv();
+    final fieldFormatValue = fieldFormatString == null
+        ? defaultVal.fieldFormat
+        : fieldFormatConv.fromJson(fieldFormatString);
+
+    final useDocCommentAsDescriptionValue =
+        json[useDocCommentAsDescriptionKey] as bool? ??
+            defaultVal.useDocCommentAsDescription;
+
+    final additionalExtensionsValue = defaultVal.additionalExtensions;
+
+    final requiredExtensionsValue =
+        json[requiredExtensionsKey] as List<String>? ??
+            defaultVal.requiredExtensions;
+
+    final config = ClassConfig(
+      createJsonConv: createJsonConvValue,
+      fieldFormat: fieldFormatValue,
+      useDocCommentAsDescription: useDocCommentAsDescriptionValue,
+      additionalExtensions: additionalExtensionsValue,
+      requiredExtensions: requiredExtensionsValue,
+    );
+    return Settings._(config);
+  }
+
+  final ClassConfig _config;
+
+  /// {@macro enum_assist.settings}
+  ClassConfig get config => _config;
 }
