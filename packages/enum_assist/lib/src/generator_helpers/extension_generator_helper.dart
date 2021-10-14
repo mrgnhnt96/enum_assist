43815e522e8ff40b653d42247f2c0102dd50a0c7
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-14 15:31:43 -0500

    feat: add check to write create json conv

--- packages/enum_assist/lib/src/generator_helpers/extension_generator_helper.dart
+++ packages/enum_assist/lib/src/generator_helpers/extension_generator_helper.dart
@@ -3,17 +3,26 @@ import 'package:enum_assist/src/templates/description_extension_template.dart';
 import 'package:enum_assist/src/templates/map_extension_template.dart';
 import 'package:enum_assist/src/templates/maybe_map_extension_template.dart';
 import 'package:enum_assist/src/templates/name_template.dart';
+import 'package:enum_assist/src/templates/serialized_extension_template.dart';
 
 /// A generator class for enum extensions
 abstract class ExtensionGeneratorHelper implements HelperCore {
   /// generates extensions code
   String generateExtensions(String Function() additionalExtensions) {
     final buffer = StringBuffer()
+      ..writeln('''
+/// Extensions for the enum $enumName''')
       ..writeln('extension ${enumName}X on $enumName {')
       ..writeln(MapTemplate(enumName, fieldNames).toString())
       ..writeln(MaybeMapTemplate(enumName, fieldNames).toString())
       ..writeln(NameTemplate(enumName, fields).toString())
-      ..writeln(DescriptionTemplate(enumName, fields).toString())
+      ..writeln(DescriptionTemplate(enumName, fields).toString());
+
+    if (config.createJsonConv) {
+      buffer.writeln(SerializedTemplate(enumName, fields).toString());
+    }
+
+    buffer
       ..writeln(additionalExtensions())
       ..writeln('}');
 
