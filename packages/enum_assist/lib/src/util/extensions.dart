8307bc91ae415f91ee4d136d4b86a4e9d84109cf
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-14 14:31:55 -0500

    refactor: remove field format to field rename

--- packages/enum_assist/lib/src/util/extensions.dart
+++ packages/enum_assist/lib/src/util/extensions.dart
@@ -1,17 +1,4 @@
 import 'package:enum_assist/src/util/string_helpers.dart';
-import 'package:enum_assist_annotation/enum_assist_annotation.dart';
-import 'package:json_annotation/json_annotation.dart';
-
-/// Extension used for [FieldFormat]
-extension FieldFormatX on FieldFormat {
-  /// Returns the [FieldFormat] as a [FieldRename]
-  FieldRename toRename() => map(
-        kebab: FieldRename.kebab,
-        snake: FieldRename.snake,
-        pascal: FieldRename.pascal,
-        none: FieldRename.none,
-      );
-}
 
 /// extension to [StringBuffer]
 extension StringBufferX on StringBuffer {
