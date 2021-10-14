adcfd57bfd4682b534e18358eca5848cd0bf5bdd
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-14 15:33:44 -0500

    refactor: add nullable

--- packages/enum_assist/lib/src/util/enum_helpers.dart
+++ packages/enum_assist/lib/src/util/enum_helpers.dart
@@ -2,9 +2,13 @@ import 'package:analyzer/dart/constant/value.dart';
 
 /// gets the value of an Enum from a Dart Object
 T? getEnumFromDartObject<T>(
-  DartObject obj,
+  DartObject? obj,
   List<T> values,
 ) {
+  if (obj == null) {
+    return null;
+  }
+
   String getName(T format) => '$format'.split('.').last;
 
   final enumValue = values.singleWhere((v) => obj.getField(getName(v)) != null);
