99f6f6e8db9d22e8b0ca52bdb3619fba6a5029e2
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-15 08:02:27 -0500

    feat: add class config test

--- packages/enum_assist/test/src/configs/class_config_test.dart
+++ packages/enum_assist/test/src/configs/class_config_test.dart
@@ -1,7 +1,25 @@
+import 'package:enum_assist/src/configs/class_config.dart';
+import 'package:enum_assist_annotation/enum_assist_annotation.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('class config ...', () async {
-    // TODO: Implement test
+  group('#defaults', () {
+    final defaults = ClassConfig.defaults;
+
+    test('createJsonConv should return true', () {
+      expect(defaults.createJsonConv, isTrue);
+    });
+
+    test('serializedFormat should return SerializedFormat.none', () {
+      expect(defaults.serializedFormat, SerializedFormat.none);
+    });
+
+    test('useDocCommentAsDescription should return true', () {
+      expect(defaults.useDocCommentAsDescription, isTrue);
+    });
+
+    test('additionalExtensions should return empty list', () {
+      expect(defaults.additionalExtensions, isEmpty);
+    });
   });
 }
