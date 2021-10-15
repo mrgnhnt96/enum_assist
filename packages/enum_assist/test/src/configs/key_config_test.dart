a64e23659e72dec7aa030d917c07833657cf39ef
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-15 08:38:21 -0500

    feat: add test for key config

--- packages/enum_assist/test/src/configs/key_config_test.dart
+++ packages/enum_assist/test/src/configs/key_config_test.dart
@@ -1,7 +1,28 @@
+import 'package:enum_assist/src/configs/key_config.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('key config ...', () async {
-    // TODO: Implement test
+  group('#defaults', () {
+    final defaults = KeyConfig.defaults;
+
+    test('name should return null', () {
+      expect(defaults.name, isNull);
+    });
+
+    test('description should return null', () {
+      expect(defaults.description, isNull);
+    });
+
+    test('serializedValue should return null', () {
+      expect(defaults.serializedValue, isNull);
+    });
+
+    test('useDocCommentAsDescription should return true', () {
+      expect(defaults.useDocCommentAsDescription, isTrue);
+    });
+
+    test('extensionValues should return empty list', () {
+      expect(defaults.extensionValues, isEmpty);
+    });
   });
 }
