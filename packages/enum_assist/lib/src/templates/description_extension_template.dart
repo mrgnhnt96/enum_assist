e04069596554088ae98b6ef113dcff305c6df728
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-15 14:16:12 -0500

    refactor: convert description template to map

--- packages/enum_assist/lib/src/templates/description_extension_template.dart
+++ packages/enum_assist/lib/src/templates/description_extension_template.dart
@@ -1,16 +1,15 @@
 import 'package:enum_assist/src/enum_field.dart';
-import 'package:enum_assist/src/templates/maybe_map_template.dart';
+import 'package:enum_assist/src/templates/map_template.dart';
 
 /// {@template enum_assist.description_template}
 /// Returns the description from the enum field.
 /// {@endtemplate}
-class DescriptionTemplate extends MaybeMapTemplate {
+class DescriptionTemplate extends MapTemplate {
   /// {@macro enum_assist.description_template}
   DescriptionTemplate(String enumName, Iterable<EnumField> fields)
       : super(
           enumName,
           fields,
-          defaultValue: null,
           getValue: (field) => field.getDescription,
           methodName: 'description',
           typeAsString: '$String?',
