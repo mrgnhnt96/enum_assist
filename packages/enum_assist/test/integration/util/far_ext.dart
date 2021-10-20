031be653f9e5a930ffbef774e77c1c08280dffbc
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-20 10:01:53 -0500

    refactor: rename file & add null classes

---
+++ packages/enum_assist/test/integration/util/far_ext.dart
@@ -0,0 +1,29 @@
+import 'package:enum_assist_annotation/enum_assist_annotation.dart';
+
+class FarMapExt extends MapExtension<double> {
+  const FarMapExt(double value)
+      : super(
+          value,
+          methodName: 'very far',
+        );
+}
+
+class NullFarMapExt extends MapExtension<double?> {
+  const NullFarMapExt([double? value])
+      : super(value, methodName: 'very far', allowNulls: true);
+}
+
+class FarMaybeExt extends MaybeExtension<double> {
+  const FarMaybeExt(double value)
+      : super(
+          value,
+          methodName: 'very far',
+          defaultValue: 12,
+        );
+}
+
+class NullFarMaybeExt extends MaybeExtension<double> {
+  const NullFarMaybeExt(double value)
+      : super(value,
+            methodName: 'very far', defaultValue: 12, allowNulls: true);
+}
