c387bca997a1f7ba7a48b4d16f5d5395a72c9240
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-21 16:55:45 -0500

    feat: create enum exception

--- packages/enum_assist/lib/src/util/exceptions.dart
+++ packages/enum_assist/lib/src/util/exceptions.dart
@@ -1,81 +1,37 @@
-/// {@template enum_assist.missing_value_exception}
-/// The exception thrown when a value is missing
+/// {@template enum_exception}
+/// The Exception thrown by EnumAssist
 /// {@endtemplate}
-class MissingValueException<T> implements Exception {
-  /// {@macro enum_assist.missing_value_exception}
-  const MissingValueException(this.key) : assert(key != null);
+class EnumException extends ArgumentError {
+  /// {@macro enum_exception}
+  EnumException({
+    required this.error,
+    required this.what,
+    required this.where,
+    required this.rule,
+    required this.fix,
+  }) : super('''
 
-  /// the key/field of the missing value
-  final T key;
+--- --- ---
+[ERROR]:  $error
+[WHERE]:  $where
+[WHAT]:   $what
+[RULE]:   $rule
+[FIX]:    $fix
+--- --- ---
+''');
 
-  @override
-  String toString() {
-    if (key is String) return key as String;
+  /// The summary of the error
+  final String error;
 
-    return 'Missing value for: $key';
-  }
-}
+  /// The enum that caused the error
+  final String where;
 
-/// {@template enum_assist.invalid_value_exception}
-/// The exception thrown when a value is null
-/// and the return type is not nullable
-/// {@endtemplate}
-class NullValueException implements Exception {
-  /// {@macro enum_assist.invalid_value_exception}
-  const NullValueException(this.key, this.method);
+  /// why the error occurred
+  final String what;
 
-  /// the key/field of the invalid value
-  final String key;
+  /// the rule that was violated
+  final String rule;
 
-  /// the method on which the error was thrown
-  final String method;
-
-  @override
-  String toString() {
-    return 'Null value for non-nullable field: $key for $method';
-  }
-}
-
-/// {@template enum_assist.missing_extension_value_exception}
-/// Thrown when an enum is annotated with an `AdditionalExtension`
-/// and the value is missing
-///
-/// __only thrown if return type is not nullable__
-/// {@endtemplate}
-class MissingExtensionValueException implements Exception {
-  /// {@macro enum_assist.missing_extension_value_exception}
-  const MissingExtensionValueException(this.key, this.extension);
-
-  /// the key/field of the invalid value
-  final String key;
-
-  /// the extension that was was expecting a value
-  final String extension;
-
-  @override
-  String toString() {
-    return '$key is missing a value annotation for $extension';
-  }
-}
-
-/// {@template enum_assist.bad_string_format_exception}
-/// Thrown when an enum is annotated with an `AdditionalExtension`
-/// and the value is missing
-///
-/// __only thrown if return type is not nullable__
-/// {@endtemplate}
-class BadStringFormatException implements Exception {
-  /// {@macro enum_assist.bad_string_format_exception}
-  const BadStringFormatException(this.key, this.value);
-
-  /// the key/field of the invalid value
-  final String key;
-
-  /// the value the was expected to be formatted
-  final String value;
-
-  @override
-  String toString() {
-    return '$key is not a valid string format: "$value"';
-  }
+  /// the fix to the error
+  final String fix;
 }
