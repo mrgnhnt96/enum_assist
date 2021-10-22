7306859645b187ee7e99334a916d10229c5af2b9
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-22 12:11:33 -0500

    refactor: create helper file for resolve

--- packages/enum_assist/lib/src/configs/extension_config.dart
+++ packages/enum_assist/lib/src/configs/extension_config.dart
@@ -11,56 +11,10 @@ import 'package:enum_assist_annotation/enum_assist_annotation.dart';
 import 'package:meta/meta.dart';
 import 'package:source_gen/source_gen.dart';
 
-const _reservedWords = {
-  // dart specific
-  'continue': '',
-  'false': '',
-  'new': '',
-  'this': '',
-  'default': '',
-  'final': '',
-  'null': '',
-  'throw': '',
-  'assert': '',
-  'finally': '',
-  'true': '',
-  'do': '',
-  'for': '',
-  'try': '',
-  'rethrow': '',
-  'else': '',
-  'if': '',
-  'return': '',
-  'var': '',
-  'break': '',
-  'enum': '',
-  'void': '',
-  'case': '',
-  'while': '',
-  'catch': '',
-  'in': '',
-  'super': '',
-  'with': '',
-  'class': '',
-  'extends': '',
-  'is': '',
-  'switch': '',
-  'const': '',
-  'bool': '',
-  'double': '',
-  'dynamic': '',
-  'int': '',
-  'num': '',
-  // enum_assist specific
-  'map': '',
-  'maybeMap': '',
-  'name': '',
-  'description': '',
-  'serialized': '',
-};
+part 'extension_config_helper.dart';
 
 /// {@template enum_assist.extension_value_config}
-/// The configuration for the additional extension.
+/// The configuration for an [Extension]
 /// {@endtemplate}
 class ExtensionConfig {
   /// {@macro enum_assist.extension_value_config}
@@ -88,31 +42,34 @@ class ExtensionConfig {
     this.allowNulls = true,
   }) : methodName = methodName.toCamelCase();
 
-  /// The name of the class that represents the value.
+  /// [Extension.methodName]
   final String methodName;
 
-  /// The name of the top level class in the [EnumKey] annotation
+  /// The class used to define the [Extension]
   final String valueClassName;
 
-  /// The type of the value.
+  /// The type of [Extension.value]
   final String valueType;
 
-  /// The value.
+  /// [Extension.value]
   final String value;
 
-  ///
+  /// [Extension.methodType]
   final MethodType methodType;
 
-  ///
+  /// [Extension.defaultValue]
   final String? defaultValue;
 
-  /// {@macro enum_assist.additional_extension.doc_comment}
+  /// [Extension.docComment]
   final String? docComment;
 
-  /// {@macro enum_assist.additional_extension.allow_nulls}
+  /// [Extension.allowNulls]
   final bool allowNulls;
 
-  /// returns the formatted [docComment]
+  /// [Extension.docComment]
+  ///
+  /// If [docComment] is `null`, returns `/// @nodoc`\
+  /// Prepends `/// ` to each line of [docComment]
   String getDocComment() {
     if (docComment == null) return '/// @nodoc';
 
@@ -135,7 +92,9 @@ value: $value
 ''';
   }
 
-  /// retrieves Map<METHOD_NAME, ExtensionConfig> from the [element]
+  /// {@macro enum_assist.extension_value_config}
+  ///
+  /// Returns Map<[methodName], [ExtensionConfig]> from the [element]
   static Map<String, ExtensionConfig> resolve(FieldElement element) {
     final object = FieldData.enumKeyChecker.getObjectFromAnnotation(element);
     final reader = ConstantReader(object);
@@ -143,187 +102,17 @@ value: $value
     final enumAndField =
         '${element.getter?.declaration}'.replaceAll(' get ', '.');
 
-    List<String> getDetailedExtension(ElementAnnotation data) {
-      const comparable = 'List<Extension<Object>> ([';
-
-      final detailsRaw = '${reader.peek('extensions')?.objectValue}';
-
-      if (!detailsRaw.contains(comparable)) return [];
-
-      final details =
-          detailsRaw.substring(comparable.length, detailsRaw.length - 2);
-
-      return details.split(', ');
-    }
-
-    List<String> getExtension(String str) {
-      if (str.isEmpty) return [];
-
-      final bracket = matchBracket(str, '(', 0);
-      if (bracket == null) return [];
-
-      final extClass = str.substring(0, bracket.start);
-      final annotation = '$extClass(${bracket.contents})';
-      final remaining = str.substring(min(bracket.end + 3, str.length));
-
-      if (remaining.isEmpty) return [annotation];
-
-      final ext = getExtension(remaining);
-
-      return [annotation, ...ext];
-    }
-
-    List<String> getExtensionAnnotation(ElementAnnotation data) {
-      const comparable = 'extensions: [';
-      const length = comparable.length;
-
-      final detailsRaw = data.toSource();
-
-      if (!detailsRaw.contains(comparable)) return [];
-
-      var details =
-          detailsRaw.substring(detailsRaw.indexOf(comparable) + length);
-
-      details = details.substring(0, details.lastIndexOf(']'));
-
-      final extensions = getExtension(details);
-
-      return extensions;
-    }
-
-    String checkMethodNameForReservedWords(String methodName) {
-      if (_reservedWords.containsKey(methodName)) {
-        final replacement = '${methodName}Ext';
-        GeneratorHelper.addToWarnings(
-          warning: 'Reserved Word in Method Name',
-          where: enumAndField,
-          what: 'The method name "$methodName" is a reserved word '
-              'and it cannot be used as a method name',
-          action: '"$methodName" will be RENAMED to "$replacement"',
-        );
-        return replacement;
-      }
-
-      return methodName;
-    }
-
-    Map<String, ExtensionConfig> getClassesAndValues(
-        List<String> annotations, List<String> details) {
+    Map<String, ExtensionConfig> getConfiguration(List<String> annotations) {
       final configs = <String, ExtensionConfig>{};
 
-      /// Map<CLASS_NAME, METHOD_NAME>
+      /// Map<[ExtensionConfig.valueClassName], [ExtensionConfig.methodName]>
       final classToMethod = <String, String>{};
 
-      void checkIfMethodNameIsUniqueToClass(
-        String className,
-        String methodName,
-      ) {
-        void checkIfMethodNameIsUnique() {
-          if (!classToMethod.containsValue(methodName)) {
-            classToMethod[className] = methodName;
-            return;
-          }
-
-          final classes = classToMethod.entries
-              .where((entry) => entry.value == methodName)
-              .map((entry) => entry.key)
-              .toList();
-
-          if (classes.length == 1) return;
-
-          final classesInError = classes.join(' & ');
-
-          throw EnumException(
-            error: 'Duplicated Method Name',
-            where: enumAndField,
-            what: 'The method "$methodName" has been '
-                'defined in $classesInError',
-            rule: 'Method names must be unique.',
-            fix: 'Double Check $classesInError, and change one of the '
-                'method names from "$methodName" and try again',
-          );
-        }
-
-        // if class doesn't exist, add it and move on
-        if (!classToMethod.containsKey(className)) {
-          classToMethod[className] = methodName;
-
-          checkIfMethodNameIsUnique();
-          return;
-        }
-
-        // get method name and check that is matches [methodName]
-        final existingMethodName = classToMethod[className];
-        if (existingMethodName == methodName) return;
-
-        // [methodName] did not match [existingMethodName]
-        throw EnumException(
-          error: 'Duplicated Method Name',
-          where: enumAndField,
-          what: '$className has different method names assigned, '
-              '"$existingMethodName" and "$methodName" were found',
-          rule: 'Method names must be unique for each `enum`',
-          fix: 'Double Check $className, and change one of the '
-              'method names from "$methodName" or $existingMethodName '
-              'and try again',
-        );
-      }
-
-      // Map<TOP_LEVEL_NAME, VALUE>
-      final topLevel = <String, String>{};
-
-      /// does nothing if key is not found
-      ///
-      /// throws error if key is found,
-      /// all methodNames must be unique
-      void checkIfExtClassExists(String extClassName) {
-        if (!topLevel.containsKey(extClassName)) return;
-
-        throw EnumException(
-          error: 'Duplicated Extension Declaration',
-          where: enumAndField,
-          what: 'The extension "$extClassName" has been '
-              'declared already for $enumAndField',
-          rule: 'Extensions can only be declared '
-              'once for a single enum field',
-          fix: 'Double check the argument '
-              '`extensions: [...]` in $enumAndField '
-              '& change or remove a `$extClassName` declaration',
-        );
-      }
-
-      for (final extClass in annotations) {
-        final parenthesisPosition = extClass.indexOf('(');
-
-        // skip extensions with an empty list
-        if (parenthesisPosition == -1) continue;
-
-        final name = extClass.substring(0, parenthesisPosition);
-        checkIfExtClassExists(name);
-
-        var value = extClass.substring(
-          parenthesisPosition + 1,
-          extClass.length - 1,
-        );
-
-        if (value == '') {
-          topLevel[name] = 'null';
-          continue;
-        }
-
-        // splits the arguments by ": "
-        final namedArgPattern = RegExp(r'(\w+: )(?![^(]*\))');
-        if (namedArgPattern.hasMatch(value)) {
-          // can't really know exactly where the value is,
-          // so just assume its the first one...
-          final args = value.split(namedArgPattern)
-            ..removeWhere((e) => e.isEmpty);
-
-          value = args[0];
-        }
-
-        topLevel[name] = value;
-      }
+      /// Map<[ExtensionConfig.valueClassName], [ExtensionConfig.value]>
+      final extensionDeclarations = _checkForDuplicateDeclarations(
+        annotations,
+        enumAndField,
+      );
 
       final extensions = reader.peek('extensions')?.listValue;
 
@@ -362,7 +151,7 @@ value: $value
             getEnumFromDartObject(methodTypeObj, MethodType.values);
         final fallbackValue = extension.peek('value')?.objectValue;
 
-        var value = topLevel[extensionClassName];
+        var value = extensionDeclarations[extensionClassName];
         final isValueNull = value == 'null';
         final isFallbackValueNull = fallbackValue?.isNull ?? true;
 
@@ -370,41 +159,34 @@ value: $value
           value = '$extensionClassName().value';
         }
 
-        // should never throw, as all fields are required
-        // non-nullable fields
-        //
-        // but just in case (also helps with null safety!)
-        if (methodName == null ||
-            methodType == null ||
-            allowNulls == null ||
-            value == null) {
-          final missingFields = <String>[];
+        // if missing value, throw error
+        _throwIfMissingField(
+          value: value,
+          methodName: methodName,
+          methodType: methodType,
+          allowNulls: allowNulls,
+          enumAndField: enumAndField,
+          extensionClassName: extensionClassName,
+        );
 
-          if (methodName == null) missingFields.add('methodName');
-          if (methodType == null) missingFields.add('methodType');
-          if (allowNulls == null) missingFields.add('allowNulls');
-          if (value == null) missingFields.add('value');
+        // Force non null values
+        methodName!;
+        value!;
+        methodType!;
+        allowNulls!;
 
-          final whatsMissing = missingFields.join(', ');
+        // make sure [methodName] is not duplicated
+        // between extension class declarations
+        _checkIfMethodNameIsUniqueToClass(
+          extensionClassName,
+          methodName,
+          classToMethod,
+          enumAndField,
+        );
 
-          throw EnumException(
-            error: 'Missing Required Fields',
-            where: enumAndField,
-            rule: 'All methods must have a method name [methodName], '
-                'method type, a value, and whether `null`s may '
-                'be a return value [allowNulls]',
-            what: 'The extension "$extensionClassName" is missing '
-                'the following fields: $whatsMissing',
-            fix: 'Double check $extensionClassName, found in the argument '
-                "`extensions: [...]` from $enumAndField's annotation. "
-                'Make sure that the fields: $whatsMissing, '
-                'are properly assigned',
-          );
-        }
+        // check for reserved word and update if necessary
+        methodName = _checkMethodNameForReservedWords(methodName, enumAndField);
 
-        checkIfMethodNameIsUniqueToClass(extensionClassName, methodName);
-
-        methodName = checkMethodNameForReservedWords(methodName);
         configs[methodName] = ExtensionConfig(
           methodName: methodName,
           valueClassName: extensionClassName,
@@ -420,15 +202,14 @@ value: $value
       return configs;
     }
 
-    final extensionsMap = <String, ExtensionConfig>{};
+    final configs = <String, ExtensionConfig>{};
 
     for (final data in element.metadata) {
-      final annotation = getExtensionAnnotation(data);
-      final details = getDetailedExtension(data);
+      final annotation = _getExtensionAnnotation(data);
 
-      extensionsMap.addAll(getClassesAndValues(annotation, details));
+      configs.addAll(getConfiguration(annotation));
     }
 
-    return extensionsMap;
+    return configs;
   }
 }
---
+++ packages/enum_assist/lib/src/configs/extension_config_helper.dart
@@ -0,0 +1,295 @@
+part of 'extension_config.dart';
+
+const _reservedWords = {
+  // dart specific
+  'continue': '',
+  'false': '',
+  'new': '',
+  'this': '',
+  'default': '',
+  'final': '',
+  'null': '',
+  'throw': '',
+  'assert': '',
+  'finally': '',
+  'true': '',
+  'do': '',
+  'for': '',
+  'try': '',
+  'rethrow': '',
+  'else': '',
+  'if': '',
+  'return': '',
+  'var': '',
+  'break': '',
+  'enum': '',
+  'void': '',
+  'case': '',
+  'while': '',
+  'catch': '',
+  'in': '',
+  'super': '',
+  'with': '',
+  'class': '',
+  'extends': '',
+  'is': '',
+  'switch': '',
+  'const': '',
+  'bool': '',
+  'double': '',
+  'dynamic': '',
+  'int': '',
+  'num': '',
+  // enum_assist specific
+  'map': '',
+  'maybeMap': '',
+  'name': '',
+  'description': '',
+  'serialized': '',
+};
+
+/// Recursively returns the declared [ExtensionConfig.valueClassName] and
+/// value from [EnumKey.extensions]
+///
+/// An extension consists of:
+/// - [ExtensionConfig.valueClassName]
+/// - [ExtensionConfig.value]?
+///   - Returns [ExtensionConfig.value] if [Extension.value]
+///     is an assigned argument
+///
+/// example:
+///
+/// Where `FactorsExt([List<int> values = [1, 2, 3]])`
+///
+/// Results can be:\
+/// `FactorsExt([1, 3])`, `FactorsExt()`
+List<String> _getExtensions(String str) {
+  if (str.isEmpty) return [];
+
+  final bracket = matchBracket(str, '(', 0);
+  if (bracket == null) return [];
+
+  final extClass = str.substring(0, bracket.start);
+  final annotation = '$extClass(${bracket.contents})';
+  final remaining = str.substring(min(bracket.end + 3, str.length));
+
+  if (remaining.isEmpty) return [annotation];
+
+  final ext = _getExtensions(remaining);
+
+  return [annotation, ...ext];
+}
+
+/// Gets the literal extension declaration from the annotation
+/// and returns them as a list
+///
+/// example:
+///
+/// Annotation:\
+/// `@EnumKey(extensions: [ToMap({'thirty-three' : 33}), IsImportant(true)])`
+///
+/// Results in:\
+/// `[ToMap({'thirty-three' : 33}), IsImportant(true)]`
+List<String> _getExtensionAnnotation(ElementAnnotation data) {
+  const comparable = 'extensions: [';
+  const length = comparable.length;
+
+  final detailsRaw = data.toSource();
+
+  if (!detailsRaw.contains(comparable)) return [];
+
+  var details = detailsRaw.substring(detailsRaw.indexOf(comparable) + length);
+
+  details = details.substring(0, details.lastIndexOf(']'));
+
+  final extensions = _getExtensions(details);
+
+  return extensions;
+}
+
+/// Checks the [ExtensionConfig.methodName] for any reserved words
+///
+/// If the [ExtensionConfig.methodName] is a reserved word,
+/// it will be renamed by appending `Ext`
+String _checkMethodNameForReservedWords(
+    String methodName, String enumAndField) {
+  if (_reservedWords.containsKey(methodName)) {
+    final replacement = '${methodName}Ext';
+    GeneratorHelper.addToWarnings(
+      warning: 'Reserved Word in Method Name',
+      where: enumAndField,
+      what: 'The method name "$methodName" is a reserved word '
+          'and it cannot be used as a method name',
+      action: '"$methodName" will be RENAMED to "$replacement"',
+    );
+    return replacement;
+  }
+
+  return methodName;
+}
+
+void _checkIfMethodNameIsUniqueToClass(
+  String className,
+  String methodName,
+  Map<String, String> map,
+  String enumAndField,
+) {
+  void checkIfMethodNameIsUnique() {
+    if (!map.containsValue(methodName)) {
+      map[className] = methodName;
+      return;
+    }
+
+    final classes = map.entries
+        .where((entry) => entry.value == methodName)
+        .map((entry) => entry.key)
+        .toList();
+
+    if (classes.length == 1) return;
+
+    final classesInError = classes.join(' & ');
+
+    throw EnumException(
+      error: 'Duplicated Method Name',
+      where: enumAndField,
+      what: 'The method "$methodName" has been '
+          'defined in $classesInError',
+      rule: 'Method names must be unique.',
+      fix: 'Double Check $classesInError, and change one of the '
+          'method names from "$methodName" and try again',
+    );
+  }
+
+  // if class doesn't exist, add it and move on
+  if (!map.containsKey(className)) {
+    map[className] = methodName;
+
+    checkIfMethodNameIsUnique();
+    return;
+  }
+
+  // get method name and check that is matches [methodName]
+  final existingMethodName = map[className];
+  if (existingMethodName == methodName) return;
+
+  // [methodName] did not match [existingMethodName]
+  throw EnumException(
+    error: 'Duplicated Method Name',
+    where: enumAndField,
+    what: '$className has different method names assigned, '
+        '"$existingMethodName" and "$methodName" were found',
+    rule: 'Method names must be unique for each `enum`',
+    fix: 'Double Check $className, and change one of the '
+        'method names from "$methodName" or $existingMethodName '
+        'and try again',
+  );
+}
+
+/// __Returns__ Map<[ExtensionConfig.valueClassName], [ExtensionConfig.value]>
+///
+/// Checks if the [ExtensionConfig.valueClassName] already exists
+///
+/// If it does exist, nothing is expected to happen
+/// because the [ExtensionConfig.valueClassName] should be unique
+/// for each annotation. There __cannot__ be duplicates
+///
+/// throws an [EnumException] if another [ExtensionConfig.valueClassName]
+/// already exists
+Map<String, String> _checkForDuplicateDeclarations(
+    List<String> annotations, String enumAndField) {
+  final topLevel = <String, String>{};
+
+  void throwIfDuplicate(
+    String extClassName,
+  ) {
+    if (!topLevel.containsKey(extClassName)) return;
+
+    throw EnumException(
+      error: 'Duplicated Extension Declaration',
+      where: enumAndField,
+      what: 'The extension "$extClassName" has been '
+          'declared already for $enumAndField',
+      rule: 'Extensions can only be declared '
+          'once for a single enum field',
+      fix: 'Double check the argument '
+          '`extensions: [...]` in $enumAndField '
+          '& change or remove a `$extClassName` declaration',
+    );
+  }
+
+  for (final extClass in annotations) {
+    final parenthesisPosition = extClass.indexOf('(');
+
+    // skip extensions with an empty list
+    if (parenthesisPosition == -1) continue;
+
+    final name = extClass.substring(0, parenthesisPosition);
+
+    throwIfDuplicate(name);
+
+    var value = extClass.substring(
+      parenthesisPosition + 1,
+      extClass.length - 1,
+    );
+
+    if (value == '') {
+      topLevel[name] = 'null';
+      continue;
+    }
+
+    // splits the arguments by ": "
+    final namedArgPattern = RegExp(r'(\w+: )(?![^(]*\))');
+    if (namedArgPattern.hasMatch(value)) {
+      // can't really know exactly where the value is,
+      // so just assume its the first one...
+      final args = value.split(namedArgPattern)..removeWhere((e) => e.isEmpty);
+
+      value = args[0];
+    }
+
+    topLevel[name] = value;
+  }
+
+  return topLevel;
+}
+
+void _throwIfMissingField({
+  required String? methodName,
+  required MethodType? methodType,
+  required bool? allowNulls,
+  required String? value,
+  required String enumAndField,
+  required String extensionClassName,
+}) {
+  // should never throw, as all fields are required
+  // non-nullable fields
+  //
+  // but just in case (also helps with null safety!)
+  if (methodName == null ||
+      methodType == null ||
+      allowNulls == null ||
+      value == null) {
+    final missingFields = <String>[];
+
+    if (methodName == null) missingFields.add('methodName');
+    if (methodType == null) missingFields.add('methodType');
+    if (allowNulls == null) missingFields.add('allowNulls');
+    if (value == null) missingFields.add('value');
+
+    final whatsMissing = missingFields.join(', ');
+
+    throw EnumException(
+      error: 'Missing Required Fields',
+      where: enumAndField,
+      rule: 'All methods must have a method name [methodName], '
+          'method type, a value, and whether `null`s may '
+          'be a return value [allowNulls]',
+      what: 'The extension "$extensionClassName" is missing '
+          'the following fields: $whatsMissing',
+      fix: 'Double check $extensionClassName, found in the argument '
+          "`extensions: [...]` from $enumAndField's annotation. "
+          'Make sure that the fields: $whatsMissing, '
+          'are properly assigned',
+    );
+  }
+}
