9c220e43412b63b3458460564021eb6decd6d2ac
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-22 10:37:08 -0500

    refactor: remove comments

--- packages/enum_assist/example/lib/enum_assist_example.dart
+++ packages/enum_assist/example/lib/enum_assist_example.dart
@@ -2,46 +2,6 @@ import 'package:enum_assist_annotation/enum_assist_annotation.dart';
 
 part 'enum_assist_example.ge.dart';
 
-//! generates the following methods by default:
-// - map
-//    - is the equivalent to pattern matching
-//
-// - maybeMap
-//    - is the equivalent to pattern matching,
-//        but doesn't require all callbacks to be specified
-//
-// - name
-//    - returns the name of the enum value
-//    - Formats the field to capital case unless overriden with @EnumKey.name
-//
-// - description
-//    - returns the description of the enum value
-//    - Uses the doc comment of the field unless overriden with @EnumKey.description
-//        or @EnumAssist.useDocCommentAsDescription is set to false
-//        (in the annotation or build.yaml file)
-//    - Uses the @EnumKey.description if overriden in annotation
-//
-// - serialized
-//    - returns the serialized value of the enum value, the default
-//        format is the name of the enum value
-//    - Uses the @EnumKey.serialized if overriden in annotation
-//    - Can be formatted via @EnumAssist.serializedFormat
-//        (in annotation or build.yaml file)
-//
-// -----------
-//
-// generates the following classes by default:
-// - ExampleConv
-//    - Uses json_serializable `JsonConverter` to convert to and from json
-//    - Can be used to annotate fields in model classes
-//        e.g. @ExampleConv()
-//
-// - _ExampleNullableConv
-//    - Uses json_serializable `JsonConverter` to convert to and from json
-//        with nullable values
-//    - Can be used to annotate fields in model classes
-//        e.g. @ExampleConv.nullable
-
 @EnumAssist(
   createJsonConv: true, // default
   serializedFormat: SerializedFormat.none, // default
@@ -65,17 +25,17 @@ enum Example {
         null, // default (uses nothing because [useDocCommentAsDescription] is false)
     extensions: [], // default
     serializedValue: 'two', // default
-    useDocCommentAsDescription: false, // overriden
+    useDocCommentAsDescription: false, // overridden
   )
   // two description
   two,
 
   @EnumKey(
-    name: 'Threeeeee', // overriden
-    description: 'three is the best', // overriden
+    name: 'Threeeeee', // overridden
+    description: 'three is the best', // overridden
     extensions: [], // default
-    serializedValue: '3', // overriden
-    useDocCommentAsDescription: false, // overriden
+    serializedValue: '3', // overridden
+    useDocCommentAsDescription: false, // overridden
   )
 
   /// three description
