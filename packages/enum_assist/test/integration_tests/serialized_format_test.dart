aad4a76abc3b64eec54384fab4496dec63d4daa7
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-25 19:03:36 -0500

    refactor: update test names

--- packages/enum_assist/test/integration_tests/serialized_format_test.dart
+++ packages/enum_assist/test/integration_tests/serialized_format_test.dart
@@ -18,79 +18,79 @@ void main() {
     expect(errorResult.errors, isEmpty);
   });
 
-  test('name should return camel case', () {
+  test('should return camel case', () {
     const enumValue = Camel.formatEnum;
 
     expect(enumValue.serialized, 'formatEnum');
   });
 
-  test('name should return capital case', () {
+  test('should return capital case', () {
     const enumValue = Capital.formatEnum;
 
     expect(enumValue.serialized, 'Format Enum');
   });
 
-  test('name should return constant case', () {
+  test('should return constant case', () {
     const enumValue = Constant.formatEnum;
 
     expect(enumValue.serialized, 'FORMAT_ENUM');
   });
 
-  test('name should return dot case', () {
+  test('should return dot case', () {
     const enumValue = Dot.formatEnum;
 
     expect(enumValue.serialized, 'format.enum');
   });
 
-  test('name should return header case', () {
+  test('should return header case', () {
     const enumValue = Header.formatEnum;
 
     expect(enumValue.serialized, 'Format-Enum');
   });
 
-  test('name should return kebab case', () {
+  test('should return kebab case', () {
     const enumValue = Kebab.formatEnum;
 
     expect(enumValue.serialized, 'format-enum');
   });
 
-  test('name should return no case', () {
+  test('should return no case', () {
     const enumValue = No.formatEnum;
 
     expect(enumValue.serialized, 'format enum');
   });
 
-  test('name should return none case', () {
+  test('should return none case', () {
     const enumValue = None.formatEnum;
 
     expect(enumValue.serialized, 'formatEnum');
   });
 
-  test('name should return pascal case', () {
+  test('should return pascal case', () {
     const enumValue = Pascal.formatEnum;
 
     expect(enumValue.serialized, 'FormatEnum');
   });
 
-  test('name should return path case', () {
+  test('should return path case', () {
     const enumValue = Path.formatEnum;
 
     expect(enumValue.serialized, 'format/enum');
   });
 
-  test('name should return sentence case', () {
+  test('should return sentence case', () {
     const enumValue = Sentence.formatEnum;
 
     expect(enumValue.serialized, 'Format enum');
   });
 
-  test('name should return snake case', () {
+  test('should return snake case', () {
     const enumValue = Snake.formatEnum;
 
     expect(enumValue.serialized, 'format_enum');
   });
 
-  test('name should return swap case', () {
+  test('should return swap case', () {
     const enumValue = Swap.formatEnum;
 
     expect(enumValue.serialized, 'FORMATeNUM');
