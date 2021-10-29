2d132d9f83e740780ccae75b3f37838982658ff5
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-29 03:02:12 -0600

    wip: add todo

--- packages/enum_assist/test/builder_test.dart
+++ packages/enum_assist/test/builder_test.dart
@@ -1,7 +1,7 @@
 import 'package:test/test.dart';
 
 void main() {
-  test('builder ...', () async {
-    // TODO: Implement test
-  });
+  // TODO: add tests for settings
+  // add tests for generated code & non-generated code based on settings/config
+  test('builder ...', () async {});
 }
