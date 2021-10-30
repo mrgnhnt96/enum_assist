3c85022d7e56f69417b46c2a108fce0554802d46
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-29 18:20:07 -0600

    refactor: remove unneeded test

--- packages/enum_assist/test/builder_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:test/test.dart';
-
-void main() {
-  // TODO: add tests for settings
-  // add tests for generated code & non-generated code based on settings/config
-  test('builder ...', () async {});
-}
