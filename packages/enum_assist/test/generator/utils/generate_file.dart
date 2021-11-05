8d7273301d4ee5630b96e1f21cdde72c9bd2315e
Morgan Hunt <mrgnhnt96@gmail.com>
2021-11-05 07:34:00 -0500

    refactor: isolate get gen in/out puts

--- packages/enum_assist/test/generator/utils/generate_file.dart
+++ packages/enum_assist/test/generator/utils/generate_file.dart
@@ -5,35 +5,64 @@ import 'package:enum_assist/src/enum_assist.dart';
 import 'package:enum_assist/src/src.dart';
 import 'package:source_gen/source_gen.dart';
 
+Future<String> getGeneratedInput(String fileName,
+    {bool addPart = false}) async {
+  final path = 'test/generator/content/$fileName.dart';
+
+  String? part;
+
+  if (addPart) {
+    part = "part '$fileName.g.dart';";
+  }
+
+  final content = await _getFileContent(path, part);
+
+  return content;
+}
+
+Future<String> getGeneratedOutput(String fileName) async {
+  final path = 'test/generator/content/output/$fileName.dart';
+
+  final output = await _getFileContent(path);
+  const generatedByHand = '// GENERATED CODE - DO NOT MODIFY BY HAND\n';
+
+  final part = "part of '$fileName.dart';\n";
+
+  const generator = '''
+// **************************************************************************
+// EnumAssistGenerator
+// **************************************************************************''';
+
+  return [generatedByHand, part, generator, output].join('\n');
+}
+
+Future<String> _getFileContent(String path, [String? part]) async {
+  final file = File(path);
+  final content = await file.readAsString();
+
+  return content.replaceFirst(RegExp("part .*';\n"), part ?? '');
+}
+
 Future generateFile(
   String fileName, {
-  Settings? settings,
-  bool getOutput = false,
-}) =>
-    generateFiles([fileName], settings: settings, getOutput: getOutput);
-
-Future generateFiles(
-  List<String> fileNames, {
   Settings? settings,
   bool getOutput = false,
 }) async {
   final inputs = <String, String>{};
   final outputs = <String, String>{};
+  final fileBase = 'a|lib/$fileName';
 
-  for (final name in fileNames) {
-    final path = 'test/generator/content/$name.dart';
+  // If the output is requested, then we need to add the "part" to the file
+  final addPart = getOutput;
 
-    final file = File(path);
-    final content = await file.readAsString();
-    inputs['a|lib/$name.dart'] = content;
+  final content = await getGeneratedInput(fileName, addPart: addPart);
 
-    if (!getOutput) continue;
+  inputs['$fileBase.dart'] = content;
 
-    final outputPath = 'test/generator/output/$name.dart';
-    final outputFile = File(outputPath);
-    final output = await outputFile.readAsString();
+  if (getOutput) {
+    final output = await getGeneratedOutput(fileName);
 
-    outputs['a|lib/$name.g.dart'] = output;
+    outputs['$fileBase.g.dart'] = output;
   }
 
   final builder = PartBuilder(
@@ -45,6 +74,8 @@ Future generateFiles(
     builder,
     inputs,
     outputs: outputs,
+    onLog: print,
+    isInput: (String input) => input.contains(fileBase),
     reader: await PackageAssetReader.currentIsolate(),
   );
 }
