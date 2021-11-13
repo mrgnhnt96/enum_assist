de29c180ebe92c80e9d42330c0148941f05d8c29
Morgan Hunt <mrgnhnt96@gmail.com>
2021-11-12 18:59:53 -0600

    refactor: remove logger

--- packages/enum_assist/test/generator/sucessful_test.dart
+++ packages/enum_assist/test/generator/sucessful_test.dart
@@ -1,11 +1,8 @@
-import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
 import 'utils/generate_file.dart';
 
 void main() {
-  Logger.root.level = Level.ALL;
-
   test('basic', () async {
     await generateFile('basic_enum', getOutput: true);
   });
